773f2b479a8e087e337c68b4cd56ddf86836055a
Carboni Davide <davide.carboni@cpnv.ch>
2019-02-25 20:33:56 +0100

    add new route for about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ChartComponent } from './pages/chart/chart.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'chart', component: ChartComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'about', component: AboutComponent },
 ];
 
 @NgModule({
