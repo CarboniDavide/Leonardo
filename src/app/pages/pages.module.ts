790b4c0e565b5711c2fa2132e3adfc7254b9a238
Carboni Davide <davide.carboni@cpnv.ch>
2019-02-25 20:31:31 +0100

    add about component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ChartComponent } from './chart/chart.component';
 import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent, ChartComponent, ContactComponent],
+  declarations: [HomeComponent, ChartComponent, ContactComponent, AboutComponent],
   imports: [
     CommonModule
   ]
