2f9f04ae873c8d385168bb9e99915d6b4a9180cb
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-02-28 00:04:49 +0100

    include dynamic js service

--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OlympicComponent } from './olympic/olympic.component';
+import { DynamicScriptLoaderService } from 'src/app/services/dynamic-script-loader.service';
 
 @NgModule({
   declarations: [OlympicComponent],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [DynamicScriptLoaderService]
 })
 export class ChartsModule { }
