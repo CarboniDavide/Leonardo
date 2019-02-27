9abd17f9c3336214f6c60f27808869307a0517a3
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-02-28 00:07:38 +0100

    implements new olympic graph

--- src/app/pages/charts/olympic/olympic.component.ts
+++ src/app/pages/charts/olympic/olympic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DynamicScriptLoaderService } from 'src/app/services/dynamic-script-loader.service';
 
 @Component({
   selector: 'app-olympic',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OlympicComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dynamicScriptLoader: DynamicScriptLoaderService) { }
 
   ngOnInit() {
+    this.loadScripts();
+  }
+
+  private loadScripts() {
+    // You can load multiple scripts by just providing the key as argument into load method of the service
+    this.dynamicScriptLoader.load('highcharts').then(data => {
+      this.dynamicScriptLoader.load('streamgraph',).then(data => {
+        this.dynamicScriptLoader.load('series-label').then(data => {
+          this.dynamicScriptLoader.load('annotations').then(data => {
+            this.dynamicScriptLoader.load('exporting').then(data => {
+              this.dynamicScriptLoader.load('streamgraphDraw').then(data => {
+              }).catch(error => console.log(error))
+            }).catch(error => console.log(error))
+          }).catch(error => console.log(error))
+        }).catch(error => console.log(error))
+      }).catch(error => console.log(error))
+    }).catch(error => console.log(error))
   }
 
 }
