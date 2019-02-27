6ee7e6728e04d29ffc3d9325a13f2f7110c386fd
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-02-27 23:57:13 +0100

    update dependency for charts module into pages module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,11 +5,13 @@ import { ChartComponent } from './chart/chart.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { DynamicScriptLoaderService } from '../services/dynamic-script-loader.service';
+import { ChartsModule } from './charts/charts.module';
 
 @NgModule({
   declarations: [HomeComponent, ChartComponent, ContactComponent, AboutComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ChartsModule
   ],
   providers: [DynamicScriptLoaderService]
 })
