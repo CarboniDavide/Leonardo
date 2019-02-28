dc7c8f6d1020b6af95c82398ebc538de43637b36
Davide Carboni <31761218+CarboniDavide@users.noreply.github.com>
2019-02-28 01:36:55 +0100

    implements marathon chart

--- src/app/pages/charts/marathon/marathon.component.ts
+++ src/app/pages/charts/marathon/marathon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DynamicScriptLoaderService } from 'src/app/services/dynamic-script-loader.service';
 
 @Component({
   selector: 'app-marathon',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarathonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dynamicScriptLoader: DynamicScriptLoaderService) { }
 
   ngOnInit() {
+    this.loadScripts();
   }
 
+  private loadScripts() {
+    // You can load multiple scripts by just providing the key as argument into load method of the service
+    this.dynamicScriptLoader.load('highcharts').then(data => {
+      this.dynamicScriptLoader.load('parallel-axes',).then(data => {
+        this.dynamicScriptLoader.load('spline').then(data => {
+        }).catch(error => console.log(error))
+      }).catch(error => console.log(error))
+    }).catch(error => console.log(error))
+  }
 }
