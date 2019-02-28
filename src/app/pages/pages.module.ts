9543872f7d7d29575c09734cab7fc751935bc24b
Carboni Davide <davide.carboni@cpnv.ch>
2019-02-28 22:35:13 +0100

    delete chart component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { ChartComponent } from './chart/chart.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
 import { DynamicScriptLoaderService } from '../services/dynamic-script-loader.service';
 import { ChartsModule } from './charts/charts.module';
 
 @NgModule({
-  declarations: [HomeComponent, ChartComponent, ContactComponent, AboutComponent],
+  declarations: [HomeComponent, ContactComponent, AboutComponent],
   imports: [
     CommonModule,
     ChartsModule
