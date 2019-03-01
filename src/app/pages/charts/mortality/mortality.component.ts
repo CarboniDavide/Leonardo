1c07e233c59e50f0f23787913717d3c88966a71a
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-01 22:42:26 +0100

    add new mortality component

---
+++ src/app/pages/charts/mortality/mortality.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mortality',
+  templateUrl: './mortality.component.html',
+  styleUrls: ['./mortality.component.scss']
+})
+export class MortalityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
