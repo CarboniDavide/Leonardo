c41727e1c80ac796b9469d816dcabfc222f0f3c3
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-01 22:38:50 +0100

    add new population route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { OlympicComponent } from './pages/charts/olympic/olympic.component';
 import { MarathonComponent } from './pages/charts/marathon/marathon.component';
+import { PopulationComponent } from './pages/charts/population/population.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'chart', children: [
     { path: 'olympic',component: OlympicComponent },
-    { path: 'marathon',component: MarathonComponent }
+    { path: 'marathon',component: MarathonComponent },
+    { path: 'population',component: PopulationComponent }
   ]},
   { path: 'contact', component: ContactComponent },
   { path: 'about', component: AboutComponent },
