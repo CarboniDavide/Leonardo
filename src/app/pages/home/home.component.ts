455ca27fd963b03a6dc492fab8cef026e10894ab
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-01 20:41:01 +0100

    implements home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss','./../../../assets/fonts/roboto.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  onClickMe() {
+    this.router.navigateByUrl('/chart/olympic');
   }
 
 }
