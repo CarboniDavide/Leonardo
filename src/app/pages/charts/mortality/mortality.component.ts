31b039d44b53a3ea1334c135182eb3e1bf6602e2
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-01 23:40:36 +0100

    styling new mortality page

--- src/app/pages/charts/mortality/mortality.component.ts
+++ src/app/pages/charts/mortality/mortality.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DynamicScriptLoaderService } from 'src/app/services/dynamic-script-loader.service';
 
 @Component({
   selector: 'app-mortality',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MortalityComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dynamicScriptLoader: DynamicScriptLoaderService) { }
 
   ngOnInit() {
+    this.loadScripts();
+  }
+
+  private loadScripts() {
+    // You can load multiple scripts by just providing the key as argument into load method of the service
+    this.dynamicScriptLoader.load('highcharts').then(data => {
+      this.dynamicScriptLoader.load('heatmap',).then(data => {
+        this.dynamicScriptLoader.load('treemap').then(data => {
+          this.dynamicScriptLoader.load('treemapDraw').then(data => {
+          }).catch(error => console.log(error))
+        }).catch(error => console.log(error))
+      }).catch(error => console.log(error))
+    }).catch(error => console.log(error))
   }
 
 }
