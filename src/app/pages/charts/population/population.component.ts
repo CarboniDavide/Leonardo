dab39bd1c0103e9452b3f1a1e5b32ef3fbc83b2f
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-01 22:40:18 +0100

    styling the new population chart

--- src/app/pages/charts/population/population.component.ts
+++ src/app/pages/charts/population/population.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DynamicScriptLoaderService } from 'src/app/services/dynamic-script-loader.service';
 
 @Component({
   selector: 'app-population',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopulationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dynamicScriptLoader: DynamicScriptLoaderService) { }
 
   ngOnInit() {
+    this.loadScripts();
+  }
+
+  private loadScripts() {
+    // You can load multiple scripts by just providing the key as argument into load method of the service
+    this.dynamicScriptLoader.load('highcharts').then(data => {
+      this.dynamicScriptLoader.load('sunburst',).then(data => {
+        this.dynamicScriptLoader.load('exporting').then(data => {
+          this.dynamicScriptLoader.load('sunburstDraw').then(data => {
+          }).catch(error => console.log(error))
+        }).catch(error => console.log(error))
+      }).catch(error => console.log(error))
+    }).catch(error => console.log(error))
   }
 
 }
