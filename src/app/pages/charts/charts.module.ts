5b9f1d9133b2b6aab492edad9a79a988bcddcd93
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-02 12:26:26 +0100

    add highchart module

--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -5,6 +5,10 @@ import { DynamicScriptLoaderService } from 'src/app/services/dynamic-script-load
 import { MarathonComponent } from './marathon/marathon.component';
 import { PopulationComponent } from './population/population.component';
 import { MortalityComponent } from './mortality/mortality.component';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+import * as heatmap from 'highcharts/modules/heatmap.src';
+import * as treemap from 'highcharts/modules/treemap.src';
+
 
 @NgModule({
   declarations: [OlympicComponent, MarathonComponent, PopulationComponent, MortalityComponent],
