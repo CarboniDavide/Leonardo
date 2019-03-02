05b544a496baf90f69073cb655ff69435f647aee
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-02 08:10:14 +0100

    using highchart package for population

--- src/app/pages/charts/charts.module.ts
+++ src/app/pages/charts/charts.module.ts
@@ -6,11 +6,19 @@ import { MarathonComponent } from './marathon/marathon.component';
 import { PopulationComponent } from './population/population.component';
 import { MortalityComponent } from './mortality/mortality.component';
 
+import * as exporting from 'highcharts/modules/exporting.src';
+import * as sunburst from 'highcharts/modules/sunburst.src';
+import { ChartModule, HIGHCHARTS_MODULES } from 'angular-highcharts';
+
 @NgModule({
   declarations: [OlympicComponent, MarathonComponent, PopulationComponent, MortalityComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ChartModule
   ],
-  providers: [DynamicScriptLoaderService]
+  providers: [
+    DynamicScriptLoaderService,
+    { provide: HIGHCHARTS_MODULES, useFactory: () => [ exporting, sunburst ] }
+  ]
 })
 export class ChartsModule { }
