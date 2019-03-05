77af0e05442c8a7aff9579f1f8485e6fe9d760fe
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-05 19:15:49 +0100

    delete contact compnent from page module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { ContactComponent } from './contact/contact.component';
 import { DynamicScriptLoaderService } from '../services/dynamic-script-loader.service';
 import { ChartsModule } from './charts/charts.module';
 import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [HomeComponent, ContactComponent, AboutComponent],
+  declarations: [HomeComponent, AboutComponent],
   imports: [
     CommonModule,
     ChartsModule
