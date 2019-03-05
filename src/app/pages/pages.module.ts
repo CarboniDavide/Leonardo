7a2df4fa1be3cfd04c0fd20a2df3c851a1fee741
Carboni Davide <davide.carboni@cpnv.ch>
2019-03-05 18:44:42 +0100

    delete about component

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,12 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
-import { AboutComponent } from './about/about.component';
 import { DynamicScriptLoaderService } from '../services/dynamic-script-loader.service';
 import { ChartsModule } from './charts/charts.module';
 
 @NgModule({
-  declarations: [HomeComponent, ContactComponent, AboutComponent],
+  declarations: [HomeComponent, ContactComponent],
   imports: [
     CommonModule,
     ChartsModule
